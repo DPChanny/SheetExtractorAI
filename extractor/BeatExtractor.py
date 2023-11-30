e794cda50be6347330513f60b2e8355a6ac1d9c3
DPChanny <kyoungchan0516@gmail.com>
2023-11-30 11:55:31 +0900

    load_weights error fix

--- extractor/BeatExtractor.py
+++ extractor/BeatExtractor.py
@@ -1,4 +1,8 @@
+from os import makedirs
+from os.path import join
+
 from keras import Sequential
+from keras.callbacks import EarlyStopping
 from keras.layers import LSTM, Dense, Softmax, Bidirectional
 from keras.utils import to_categorical
 from matplotlib.axes import Axes
@@ -7,7 +11,6 @@ from numpy import argmax, array
 from pandas import DataFrame
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import LabelEncoder
-from tensorflow.python.keras.callbacks import EarlyStopping
 
 from Public import (RESULT, SOURCE, FIG_HEIGHT, BeatType, Beat, BeatState, STFTFeature,
                     BeatStateDataFrameColumn,
@@ -153,25 +156,30 @@ def extract_beats(sample: Sample,
 class BeatStateExtractor:
     def __init__(self, wing_length: int = 5):
         self.wing_length = wing_length
+
         self.model = Sequential()
-        self.model.add(Bidirectional(LSTM(units=self.wing_length * 2 + 1,
-                                          input_shape=[self.wing_length * 2 + 1, 1])))
+        self.model.add(Bidirectional(LSTM(units=self.wing_length * 2 + 1)))
         self.model.add(Dense(units=self.wing_length * 2 + 1))
         self.model.add(Dense(units=3))
         self.model.add(Softmax())
         self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        self.model.build([self.wing_length * 2 + 1, 1, 1])
+
         self.label_encoder = LabelEncoder()
         self.label_encoder.fit([BeatState.START.value, BeatState.MIDDLE.value, BeatState.NONE.value])
 
-    def save(self, directory: str, beat_extractor_name: str, log: bool = False):
+    def save(self, directory: list[str], beat_state_extractor_name: str, log: bool = False):
+        directory = join(*directory)
         if log:
-            print("Saving " + beat_extractor_name)
-        self.model.save_weights("./" + RESULT + "/" + directory + "/" + beat_extractor_name + "/model")
+            print("Saving " + join(*[RESULT, directory, beat_state_extractor_name + ".bse.weights.h5"]))
+        makedirs(join(*[RESULT, directory]), exist_ok=True)
+        self.model.save_weights(join(*[RESULT, directory, beat_state_extractor_name + ".bse.weights.h5"]))
 
-    def load(self, directory: str, beat_extractor_name: str, log: bool = False):
+    def load(self, directory: list[str], beat_state_extractor_name: str, log: bool = False):
+        directory = join(*directory)
         if log:
-            print("Loading " + beat_extractor_name)
-        self.model.load_weights("./" + SOURCE + "/" + directory + "/" + beat_extractor_name + "/model")
+            print("Loading " + join(*[SOURCE, directory, beat_state_extractor_name + ".bse.weights.h5"]))
+        self.model.load_weights(join(*[SOURCE, directory, beat_state_extractor_name + ".bse.weights.h5"]))
 
     def fit(self,
             beat_data_frame: DataFrame,
@@ -252,16 +260,18 @@ def save_beat_state_extractor_history_plot(beat_state_extractor_history: dict,
                                            log: bool = False):
     fig = figure(figsize=(max(len(beat_state_extractor_history[_]) for _ in beat_state_extractor_history.keys()) / 100,
                           FIG_HEIGHT))
-    fig.suptitle("Beat Extractor History")
+    fig.suptitle("Beat State Extractor History")
 
     history_ax = fig.add_subplot(111)
 
     for key in beat_state_extractor_history.keys():
         history_ax.plot(beat_state_extractor_history[key], label=key, linewidth=0.5)
 
+    history_ax.set_xlabel("Epochs")
+
     history_ax.legend()
     history_ax.set_ylim(0, 1)
-    save_plot(directory, plot_name + ".beh", fig, log=log)
+    save_plot(directory, plot_name + ".bseh", fig, log=log)
 
 
 def plot_beats(ax: Axes, beats: list[Beat]):
